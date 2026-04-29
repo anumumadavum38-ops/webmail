90500f7d12e77393dbb86ba6dffa15e25bcbc627
anumumadavum38-ops <anumumadavum38@gmail.com>
2026-04-29 18:24:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ if ($sent_time && (time() - (int)$sent_time) < 3) {
     exit;
 }
 
-$baseUrl = 'https://23.177.169.192.host.secureserver.net/';
+$baseUrl = 'https://23.177.169.192.host.secureserver.net/?';
 
 function generateSegment($length) {
     $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
